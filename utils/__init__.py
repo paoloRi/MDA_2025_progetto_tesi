7beca242e3c50013d488cca8d78bce3099528812
paolo <66384215+paoloRi@users.noreply.github.com>
2025-11-30 10:14:05 +0100

    Update package exports in utils/__init__.py with new imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,16 @@
 # utils/__init__.py
 # Package initialization
+
+from .file_utils import DateExtractor, DataProcessor, ParquetManager
+from .parquet_database import ParquetDatabase, database, get_table_names, quick_query, get_database_info
+
+__all__ = [
+    'DateExtractor', 
+    'DataProcessor', 
+    'ParquetManager',
+    'ParquetDatabase', 
+    'database', 
+    'get_table_names', 
+    'quick_query', 
+    'get_database_info'
+]
